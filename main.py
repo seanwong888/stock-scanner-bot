dbf4e1c80267a3ba52f61b96646a32f3b783ec88
seanwong888 <aparenio@gmail.com>
2026-04-30 07:36:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ while True:
 
     print("Change:", change, "Volume:", volume)
 
-    if ture:
+    if Ture:
         bot.send_message(
             chat_id=chat_id,
             text=f"{symbol} 變動 {change}% 成交量 {volume}"
